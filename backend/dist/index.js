317e9485efc4c4873b64d9696e66dab2f089c7c0
Aaron Amoso <amosoaaron@gmail.com>
2024-09-17 23:13:12 -0600

    able to fixed the server by running npm run dev on the root folder

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,17 +1,17 @@
-import express from 'express';
-const app = express();
-// GET - get/retrieve data from db or user
-// PUT - update/modify/create data
-// POST - request data
-// DELETE - delete data
-// defines a middleware
-app.use(express.json()); // this tells the application that we will be using the json file for the incoming and outgoing requests for this app
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 //make a new request
-app.get("/he", (req, res, next) => {
-    //req-when client or user request something
-    //res-send back a response
-    //next-move onto the next middleware
-    console.log(req.body.name);
-});
-app.listen(5000, () => console.log("Server Open"));
+// app.get("/he", (req, res, next) => {
+//   //req-when client or user request something
+//   //res-send back a response
+//   //next-move onto the next middleware
+//   console.log(req.body.name);
+// Connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    // connections and listeners
+    app.listen(5000, () => console.log("Server Open & Connected to Database"));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
